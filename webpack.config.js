58c16cf891c4b999f570602dd479fa27d0115981
Eugene Croquette <welcome@croc.work>
2021-02-03 04:11:51 +0200

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,76 @@
-const path = require('path');
-const webpack = require('webpack');
-
-
-
-
-/*
- * We've enabled MiniCssExtractPlugin for you. This allows your app to
- * use css modules that will be moved into a separate CSS file instead of inside
- * one of your module entries!
- *
- * https://github.com/webpack-contrib/mini-css-extract-plugin
- *
- */
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-
-
-
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
-
-
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
-
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-
-
-
+const 
+  path = require('path'),
+  webpack = require('webpack'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  source = path.resolve(__dirname, '_src'),
+  public = path.resolve(__dirname, '_build')
+;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './_src/app.js',
+  entry: [
+    path.join(source, 'app.js'),
+    path.join(source, 'style.sass')
+  ],
 
   output: {
-    path: path.resolve(__dirname, '_build')
+    path: public
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({ filename:'style.[contenthash].css' }),
+    // new MiniCssExtractPlugin({ filename:'style.css' }),
     new HtmlWebpackPlugin({
-              template: 'index.html'
-            }),
-    new WorkboxWebpackPlugin.GenerateSW({
-            swDest: 'sw.js',
-            clientsClaim: true,
-            skipWaiting: false,
-          })
+              template: path.join(source, 'index.html')
+            })
   ],
 
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      include: [path.resolve(__dirname, '_src')],
-      loader: 'babel-loader'
-    }, {
-      test: /.(sa|sc|c)ss$/,
+    rules:
+    [
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   include: [path.resolve(__dirname, '_src')],
+      //   loader: 'babel-loader'
+      // }, 
+      {
+        test: /.(sa|sc|c)ss$/,
 
-      use: [{
-        loader: MiniCssExtractPlugin.loader
-      }, {
-        loader: "css-loader",
+        use: [
+          // {
+          // loader: MiniCssExtractPlugin.loader
+          // },
+         {
+          loader: "css-loader",
 
-        options: {
-          sourceMap: true
+          options: {
+            sourceMap: true
+          }
+        }, {
+          loader: "sass-loader",
+
+          options: {
+            sourceMap: true
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|jp?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+            filename: '[file]'
         }
-      }, {
-        loader: "sass-loader",
-
-        options: {
-          sourceMap: true
+      },
+      {
+        test: /\.(ttf|eot|woff(2)?|svg)$/,
+        type: "asset/resource",
+        generator: {
+            filename: 'fonts/[name][ext]'
         }
-      }]
-    }]
+      }
+    ]
   }
 }
