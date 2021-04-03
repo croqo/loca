accd542825313a08d95b9cf919851d06aea132d2
Eugene Croquette <croqo@outlook.com>
2021-04-03 05:30:33 +0300

    daily result

--- _src/app.js
+++ _src/app.js
@@ -1,32 +1,57 @@
 /*jshint esversion: 6 */ 
+const licence = JSON.parse(license);
 
 import file_webm from './sound/jingle.webm';
 import file_mp3 from './sound/jingle.mp3';
+const sound = createSound([file_webm, file_mp3]);
 
-import file_json from './lottie/animation-tweak.json';
+import animationJSON from './lottie/animation-tweak.json';
+const animationDATA = JSON.parse(animationJSON);
 
 import $ from "jquery";
-
 import lottie from "lottie-web";
 import {Howl} from "howler";
-import fullpage from "fullpage.js";
 
-const KEYS = JSON.parse(license);
-
-var sound = createAudio([file_webm, file_mp3]);
-var anima = createAnimation(file_json);
-anima.playSegments([0, 270]);
-console.log(sound);
-console.log(anima);
+var Player;
+// var anima = createAnimation(file_json);
 
 $(()=>{
     new fullpage("#fullpage", {
-        licenseKey: KEYS.fullpage
+        licenseKey: licence.fullpage,
+        sectionSelector: "section",
+        recordHistory: false,
+        menubar: "#navMenu",
+        fixedElements: ".navbar, is-fixed",
+        afterRender: function(){
+            $(".lottie").each((index, node)=>{
+                let anima = createAnimation(node, animationDATA);
+                anima.goToAndStop(270, true);
+
+                $(node).animate({
+                    "z-index": 2
+                }, 300, ()=>{
+                    anima.playSegments([0, 270]);
+                });
+
+                Player = {
+                    anima: anima,
+                    sound: createSound([file_webm, file_mp3])
+                }
+            })
+        },
+        afterLoad: function(){
+            console.log(this);
+            console.log(Player)
+        }
     });
+    // anima.playSegments([0, 270]);
+    // console.log(sound);
+    // console.log(anima);
+
 });
 
 
-function createAudio(assets)
+function createSound(assets)
 {
     return new Howl({
         src: assets,
@@ -35,14 +60,28 @@ function createAudio(assets)
     })
 }
 
-function createAnimation(json){
+function createAnimation(node, data){
     return lottie.loadAnimation({
-        container: $('.lottie')[0],
-        animationData: JSON.parse(json),
+        container: node,
+        animationData: data,
         autoplay: false,
         loop: false
     })
 }
+
+$(".solid").on("click", (ev)=>{
+    Player.anima.playSegments([0, 270], true);
+    Player.sound.play();
+    var it = ev.target;
+    $(it).animate({
+        opacity: .75
+    },300);
+    setTimeout(()=>{
+        $(it).animate({
+            opacity: .99
+        },1500);
+    },3000);
+})
 
 $(".acrylic").on("click", (e)=>{
     anima.playSegments([0, 270], true);
