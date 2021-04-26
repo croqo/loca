c20e09618cef279ed55c40b2924c724f289c55c3
Eugene Croquette <croqo@outlook.com>
2021-04-26 21:43:21 +0300

    mixer config update

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,16 +14,40 @@ mix.webpackConfig({
         }
       },
       {
+        test: /\.(png|jpg|jp?g|gif)$/i,
+        type: "asset",
+        generator: {
+            filename: 'images/[path][name][ext]'
+        }
+      },
+      {
+        test: /\.(ttf|eot|woff(2)?|svg)$/,
+        type: "asset",
+        generator: {
+            filename: 'fonts/[path][name][ext]'
+        }
+      },
+      {
         test: /\.(json)$/,
         type: "asset/source"
       }
     ]
+  },
+  stats: {
+    children: true
   }
 });
 mix.setResourceRoot(
       path.resolve(__dirname, 'node_modules')
     );
 mix.setPublicPath('assets');
+
+
 mix.js(
-  '_src/index.js', `js`
+    '_src/index.js', `js`
   );
+
+mix.sass(
+    '_src/index.sass', 'css'
+  );
+mix.combine('_src/css/*.css', 'style.css');
